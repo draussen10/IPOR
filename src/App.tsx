2935844d9b68650e816a0b3a671e284621134594
alexey.boev <alexey.boev@universe-data.ru>
2023-12-18 12:57:59 +0300

    Создали функцию `classNames` для удобного управления стилями

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import {Link, Route, Routes} from 'react-router-dom';
 import MainPageAsync from "./pages/MainPage/MainPage.async";
 import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
     const {toggleTheme, theme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div
+            className={classNames(
+                `app`,
+                {},
+                [theme])
+            }
+        >
             <button onClick={toggleTheme}>toggleTheme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
---
+++ src/helpers/classNames/classNames.tsx
@@ -0,0 +1,13 @@
+// не самый лучший вариант
+type Mods = Record<string, boolean | string>
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+    return [
+        cls,
+        ...additional,
+        Object
+            .entries(mods)
+            .filter(([className, value]) => !!value)
+            .map(([className, value]) => className)
+    ]
+        .join(' ')
+}
