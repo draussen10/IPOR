0cf297d0c218f8f4526f73ddfbfe578881bcd0ae
alexey.boev <alexey.boev@universe-data.ru>
2023-12-19 20:53:23 +0300

    Настраиваем EsLint. Исправляем ошибки

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+module.exports =  {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", 
+            "pragma": "React",  
+            "fragment": "Fragment",  
+            "version": "detect", 
+            "flowVersion": "0.53" 
+        },
+        "propWrapperFunctions": [
+            "forbidExtraProps",
+            {"property": "freeze", "object": "Object"},
+            {"property": "myFavoriteWrapper"},
+            {"property": "forbidExtraProps", "exact": true}
+        ],
+        "componentWrapperFunctions": [
+            "observer",
+            {"property": "styled"},
+            {"property": "observer", "object": "Mobx"},
+            {"property": "observer", "object": "<pragma>"} 
+        ],
+        "formComponents": [
+            "CustomForm",
+            {"name": "Form", "formAttribute": "endpoint"}
+        ],
+        "linkComponents": [
+            "Hyperlink",
+            {"name": "Link", "linkAttribute": "to"}
+        ]
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/no-unused-vars": "off"
+    }
+}
